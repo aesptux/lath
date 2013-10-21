85f8b08305858681e68d7fb4c1736c5f45238f28
Adrian Espinosa <aespinosamoreno@gmail.com>
2013-10-21 19:51:34 +0200

    refactor index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
-var mysql = require('mysql');    
-//var mustache = require("mustache");
+var mysql = require('mysql');
 var app = express();
 var port = 3700;
 
@@ -29,20 +28,7 @@ connection.connect();
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
-//app.get('/log', function (req,res) {
-//    var html = "";
-//    connection.query('SELECT username, message FROM chat_log', function(err, rows, fields) {
-//        if (err) throw err;
-//        query = rows;
-//        console.log(rows[0].username);
-//        for (var row in rows) {
-//            html += "<div><b>" + rows[row].username + ":</b>" + rows[row].message + "</div>";    
-//        }
-//        
-//        res.send(html);
-//    });
-//    
-//});
+
 
 // socket
 var io = require('socket.io').listen(app.listen(port));
@@ -50,18 +36,20 @@ var io = require('socket.io').listen(app.listen(port));
 
 // define events
 io.sockets.on('connection', function (socket) {
-    
+
     // when clients emit...
 
     socket.on('sendchat', function(data) {
-        io.sockets.to(socket.room).emit('updatechat', socket.username, data);  
+        io.sockets.to(socket.room).emit('updatechat', socket.username, data);
         data = data.replace(/\\/g, '\\\\');
-        connection.query('INSERT INTO chat_log(username, message, room) VALUES ("' + socket.username + '", "' + data + '", "' + socket.room + '")', function(err, rows, fields) {
+        connection.query('INSERT INTO chat_log(username, message, room) VALUES\
+         ("' + socket.username + '", "' + data + '", "' + socket.room + '")',
+         function(err, rows, fields) {
           if (err) throw err;
         });
-        
+
     });
-    
+
     socket.on('adduser', function(username) {
         socket.username = username;
         if (usernames[socket.username]) {
@@ -73,32 +61,37 @@ io.sockets.on('connection', function (socket) {
         socket.join('Calculus');
         socket.emit('updatechat', 'SERVER', 'You have connected to Calculus.');
         // broadcast to all users
-        socket.broadcast.to('Calculus').emit('updatechat', 'SERVER', username + ' has connected.');
+        socket.broadcast.to('Calculus').emit('updatechat', 'SERVER',
+            username + ' has connected.');
         // update list client side
         io.sockets.emit('updateusers', usernames);
         socket.emit('updaterooms', rooms, 'Calculus');
     });
-    
+
     socket.on('switchroom', function (newroom) {
         socket.leave(socket.room);
         socket.join(newroom);
-        socket.emit('updatechat', 'SERVER', 'You have connected to ' + newroom + '.');
-        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username + ' has left the room.');
+        socket.emit('updatechat', 'SERVER', 'You have connected to ' +
+            newroom + '.');
+        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER',
+            socket.username + ' has left the room.');
         socket.room = newroom;
-        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username + ' has joined the room');
+        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER',
+            socket.username + ' has joined the room');
         socket.emit('updaterooms', rooms, newroom);
-        
+
     });
-    
+
     // sad panda is sad with this event
     socket.on('disconnect', function() {
         delete usernames[socket.username];
         io.sockets.emit('updateusers', usernames);
         // broadcast
-        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected.');
+        socket.broadcast.emit('updatechat', 'SERVER',
+            socket.username + ' has disconnected.');
         socket.leave(socket.room);
     });
-    
+
 });
 
 console.log('Listening...');
